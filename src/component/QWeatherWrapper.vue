cdd2a21d519900ab88b9d4c18357b90942a4d28d
Neo Fu <rtugeek@gmail.com>
2024-09-12 12:02:16 +0800

    :arrow_up: upgrade version

--- src/component/QWeatherWrapper.vue
+++ src/component/QWeatherWrapper.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useStorage } from '@vueuse/core'
-import EmptyView from '@/component/EmptyView.vue'
 import { BrowserWindowApi } from '@widget-js/core'
+import EmptyView from '@/component/EmptyView.vue'
 
 defineProps({
   errorMsg: {
@@ -35,11 +35,11 @@ function viewQWeather() {
         <div class="flex flex-col gap-4 items-center">
           <span>点击设置 和风天气 API KEY</span>
           <div class="flex flex-wrap gap-1 items-center justify-center">
-            <el-button @click.stop="viewTutorial" class="w-20" type="primary">
+            <el-button class="w-20" type="primary" @click.stop="viewTutorial">
               视频教程
             </el-button>
-            <span></span>
-            <el-button @click.stop="viewQWeather" class="w-20" type="warning">
+            <span />
+            <el-button class="w-20" type="warning" @click.stop="viewQWeather">
               申请地址
             </el-button>
           </div>
